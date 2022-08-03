e2adeaa677d1ff08e9a69456feb27370c8258227
Pushpendra Singh <govindtop1@gmail.com>
2022-08-04 00:29:23 +0530

    Initial commit

---
+++ src/Component/Common.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Common = () => {
+  return (
+    <div>
+       <h1 className="text1">Styling Using Functional and Class Component</h1>
+    <button className="button1">To see styling in functional component</button>
+    <button className="button2">To see styling in class component</button>
+    </div>
+  )
+}
+
+export default Common
