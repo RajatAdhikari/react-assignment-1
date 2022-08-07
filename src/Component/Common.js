01bdda7056d59357f6efee48f56925517baacb86
Pushpendra Singh <99976461+govind124@users.noreply.github.com>
2022-08-07 13:04:16 +0530

    Update Common.js

--- src/Component/Common.js
+++ src/Component/Common.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Functional from './Functional';
+import Class from './Class';
 
 const Common = () => {
+ const [state, setState]= useState(false);
+ const [state1, setState1] = useState(false);
+ 
+ function Show(){
+  setState(!state)
+ };
+ function Show1(){
+  setState1(!state1)
+ }
+
+
   return (
     <div>
        <h1 className="text1">Styling Using Functional and Class Component</h1>
-    <button className="button1">To see styling in functional component</button>
-    <button className="button2">To see styling in class component</button>
+       
+       <button className="button1" onClick={Show}>To see styling in functional component</button>
+       {state && <Functional/>}
+       
+       <button className="button2" onClick={Show1}>To see styling in class component</button>
+       {state1 && <Class/> }
+
     </div>
   )
 }
